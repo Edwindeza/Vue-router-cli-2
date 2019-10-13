f612eb2a6fe0e663a58168a30df4bec2c3372d46
edwindeza <ejdeza@gmail.com>
2019-10-13 07:00:01 -0500

    Vue router dinamic

---
+++ src/views/Photos.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="photos">
+    <h1>Galería de Fotos</h1>
+    <router-link  :to="{name: 'photos', params: { id: item }}"
+    v-for="(item, index)  in fotos" :key="index">
+      <button>Foto {{item}}</button> 
+    </router-link>
+    <Foto />
+    <button @click="anterior"> Anterior </button>
+    <button @click="home"> Home </button>
+    <button @click="siguiente"> Siguiente </button>
+  </div>
+</template>
+
+<script>
+import  Foto from "@/components/Photo.vue"
+
+export default {
+    components:{
+      Foto
+    },
+    data(){
+      return{
+        fotos:[1,2,3]
+      }
+    },
+    methods: {
+      home(){
+        this.$router.push('/')
+      },
+      anterior(){
+        this.$router.go(-1)
+      },
+      siguiente(){
+        this.$router.go(1)
+      }
+    },
+}
+
+</script>
